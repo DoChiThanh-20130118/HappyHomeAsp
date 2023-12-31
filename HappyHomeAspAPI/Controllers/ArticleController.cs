409932c208c6e9479560b776fb12dd3751985b48
nthduc <nguyenthaiduc0212@gmail.com>
2023-12-31 22:55:34 +0700

    :hammer: Article api

---
+++ HappyHomeAspAPI/Controllers/ArticleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HappyHomeAspAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+    }
+}
---
+++ HappyHomeAspAPI/Models/Article.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace HappyHomeAspAPI.Models
+{
+    public class Article
+    {
+        private int article_id;
+
+        private int article_category_id;
+
+        private string title;
+
+        private string content;
+
+        public Article()
+        {
+        }
+
+        public Article(int article_id, int article_category_id, string title, string content)
+        {
+            this.Article_id = article_id;
+            this.Article_category_id = article_category_id;
+            this.Title = title;
+            this.Content = content;
+        }
+
+        public int Article_id { get => article_id; set => article_id = value; }
+        public int Article_category_id { get => article_category_id; set => article_category_id = value; }
+        public string Title { get => title; set => title = value; }
+        public string Content { get => content; set => content = value; }
+
+
+    }
+}
